23d2abafc0c49ad9fccc8737f60aed72dab0d932
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-14 13:53:02 -0500

    created the notes form

---
+++ src/components/NotesCard/NotesCard.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function NotesCard() {
+  const [note, setNote] = useState("");
+  const [error, setError] = useState("");
+
+  function handleChange(e) {
+    set({ ...note, [evt.target.name]: evt.target.value });
+    setError("");
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setNote(...note, [e.target.value]);
+  }
+
+  return (
+    <div>
+      <div className="form-container">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <label>Note</label>
+          <input
+            type="text"
+            name="text"
+            value={note}
+            onChange={handleChange}
+            required
+          />
+
+          <button type="submit">SAVE</button>
+        </form>
+      </div>
+      <p className="error-message">&nbsp;{error}</p>
+    </div>
+  );
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,7 +6,7 @@ import AuthPage from "../AuthPage/AuthPage";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import NavBar from "../../components/NavBar/NavBar";
-import Notes from "../../components/Notes/Notes";
+import Notes from "../Notes/Notes";
 
 export default function App() {
   const [user, setUser] = useState(getUser());
